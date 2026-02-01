29729af022cc1df2de62930cded71c35cc1b6863
Emmanuel Bourret <emmanuel.bourret@umontreal.ca>
2026-02-01 14:49:33 -0500

    MAINT: Minor docstrings changes.

---
+++ weaselytics/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""
+====================================================================
+weaselytics - A library to extract and analyse chromatographic data.
+====================================================================
+
+
+"""
+#from weaselytics import release
+
+## Release data
+#__author__ = ''
+#for author, email in release.authors.values():
+#    __author__ += author + ' <' + email + '>\n'
+#del author, email
+
+#__license__ = release.license
+#__version__ = release.version
+
+#from . import utils, peakfitting

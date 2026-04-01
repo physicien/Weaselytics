31372f09d834698474f1b3cf6a2718b65524ef01
Emmanuel Bourret <emmanuel.bourret@umontreal.ca>
2026-04-01 16:41:59 -0400

    MAINT: Simplification of the parser and update of the related docstring

--- weaselytics/parsers.py
+++ weaselytics/parsers.py
@@ -4,34 +4,66 @@
 Parser for common spectral data.
 """
 
-#import os
 import re
 from pathlib import Path
 import numpy as np
 
 class ParsedData:
     """
+    Parse a file containing x-y data organized in two columns.
+
+    Parameters
+    ----------
+    path : str
+        Path of the file to be parsed.
+
+    Attributes
+    ----------
+    path : str
+        Path of the file to be parsed.
+    data : array-like, shape (2,N)
+        Array containing the x-y data extracted from the file.
+
+    NOTE
+    ----
+    Do I really need to keep `self.path`?
+
     """
 
     def __init__ (self, path):
         self.path = path
-        self.raw = self.read_file()
-        self.data = self.read_data()
+        self.data = self.read_data(path)
 
-    def read_file(self):
-        with open(self.path,'r') as f:
-            lines = [line.rstrip() for line in f]
-        return lines
+    def read_data(self, path):
+        """
+        Read the file and
 
-    def read_data(self):
+        Parameters
+        ----------
+        path : str
+            Path of the file to be parsed.
+
+        Returns
+        -------
+        data : array-like, shape (2,N)
+            Array containing the x-y data extracted from the file.
+
+        """
         xlist = list()
         ylist = list()
         pattern = r"^([+-]?\d+\.?\d*[eE]?[-+]?\d*)\s+" + \
                     r"([+-]?\d+\.?\d*[eE]?[-+]?\d*)"
-        for line in self.raw:
-            if re.search(pattern, line):
-                xlist.append(float(line.strip().split('\t')[0]))
-                ylist.append(float(line.strip().split('\t')[1]))
-        out = np.array([xlist, ylist])
-        return out
+
+        # Read the file
+        with open(path, 'r') as f:
+            lines = [line.rstrip() for line in f]
+
+        # Extract data from each line
+        for line in lines:
+            if re.fullmatch(pattern, line):
+                xy = re.split("\s+", line)
+                xlist.append(float(xy[0]))
+                ylist.append(float(xy[1]))
+        data = np.array([xlist, ylist])
+        return data
 
